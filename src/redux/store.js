111996f592b9ab89daa314dc24d272489ce17088
Abhinav Tomar <atomar.cse18@chitkarauniversity.edu.in>
2021-03-07 15:07:19 +0530

    Remove Devtools

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,7 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore } from "redux";
 import rootReducer from "./reducers";
-import { composeWithDevTools } from 'redux-devtools-extension'
-import { logger } from 'redux-logger'
 
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(logger)));
+
+const store = createStore(rootReducer);
 
 export default store;
